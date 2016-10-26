be2cf554b2723d7887501a77b1826351f374dd5e
JaiHariOm <jaijaihariom@yahoo.com>
2016-10-26 12:59:48 +0900

    Additional licenses

--- bin/brew-update.sh
+++ bin/brew-update.sh
@@ -16,7 +16,7 @@ fi
 
 #[brew-update.sh]
 
-#Copyright (c) [year] [fullname]
+#Copyright (c) 2013 JaiHariOm (Hayashi Sakihara)
 
 #This software is released under the MIT License.
 #http://opensource.org/licenses/mit-license.php
